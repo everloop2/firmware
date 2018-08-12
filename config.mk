60223dc55dee01521af370149d31b661f1794a7a
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-08-12 13:10:37 +0200

    verbose build to debug build fails

--- config.mk
+++ config.mk
@@ -5,4 +5,4 @@ PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=ae06a650d68026530beec4196869706866c2e4b1
 SET_BUILDBOT=env
-MAKE_ARGS=
+MAKE_ARGS=V=s
